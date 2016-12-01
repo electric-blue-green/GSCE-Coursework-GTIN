eeea73baf7ebfcd3b0bf5e4e5038bc6044025dba
electric-blue-green <thomasbass2001@gmail.com>
2016-12-01 18:21:23 +0000

    Task 3 comments

--- Task 3/FINAL/task3.py
+++ Task 3/FINAL/task3.py
@@ -1,31 +1,31 @@
 __authors__ = ['Thomas Bass']
 ##    TASK 3   ##
-import sqlite3 as lite
+import sqlite3 as lite                                                                                                        ## Imports libraries
 import random
 import math
 import sys
 stockOrder = []
-con = lite.connect('dbuse.db')        ## connects to DBase
+con = lite.connect('dbuse.db')                                                                                                ## connects to DBase
 cur = con.cursor()
 def findStock(con, cur, stockOrder):
-  slq1 = 'SELECT * FROM Inventory WHERE StockAvab != TargetStock'         ## selects low stock from DBase
-  cur.execute(slq1)                                                                                       ## excesutes
-  results = cur.fetchall()                                                                                ## collects
+  slq1 = 'SELECT * FROM Inventory WHERE StockAvab != TargetStock'                                                             ## selects low stock from DBase
+  cur.execute(slq1)                                                                                                           ## excesutes
+  results = cur.fetchall()                                                                                                    ## collects
   orderList = []
   if str(results):
     print('Stock is all up to date!')
     park()
   for product in results:
-    toOrder = int(int(product[5]) - int(product[4]))                                                                                                     ## math for how many to order
-    print('Low stock! Order', toOrder, 'x', product[1], 'more to fill stock')                                                                          # # message
+    toOrder = int(int(product[5]) - int(product[4]))                                                                          ## math for how many to order
+    print('Low stock! Order', toOrder, 'x', product[1], 'more to fill stock')                                           ## message
     if product[2] == '':
       sizeName = ''
-    elif product[2] == 'Small' or product[2] == 'Medium' or product[2] == 'Large':
-      sizeName = product[2]
-    else:
-      sizeNameRaw = product[2], 'ml'
-      sizeName = "".join(sizeNameRaw)
-    stockOrderAddRaw = str(toOrder), ' x ', str(sizeName), ' ', str(product[1]), ' (GTIN: ', str(product[0]), ')'            ## creates order
+    elif product[2] == 'Small' or product[2] == 'Medium' or product[2] == 'Large':                                      ## Elif 
+      sizeName = product[2]                                                                                             ## sizeName = product[2]
+  else:                                                                                                                 ## Else
+      sizeNameRaw = product[2], 'ml'                                                                                    ## sizeNameRaw = product[2] + ml
+      sizeName = "".join(sizeNameRaw)                                                                                   ## sizeName = join sizeNameRaw ''
+    stockOrderAddRaw = (str(toOrder), ' x ', str(sizeName), ' ', str(product[1]), ' (GTIN: ', str(product[0]), ')')     ## creates order
     stockOrderAdd = "".join(stockOrderAddRaw)
     stockOrder.append(stockOrderAdd)
     orderList.append(product[0])
@@ -58,5 +58,3 @@ def park():
 ask = input('Press [s] to scan inventory stock levels\n> ')
 if ask == 's' or ask == 'S':
   findStock(con, cur, stockOrder)
-
-
